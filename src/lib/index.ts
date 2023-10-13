4f7a9bd43932cd025c7d2838b35b614194e98512
Yan <i@yinyan.fr>
2023-10-13 23:52:55 +0200

    feat: login with telegram

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,8 +2,10 @@ import hmacSHA256 from 'crypto-js/hmac-sha256';
 import sha256 from 'crypto-js/sha256';
 import Hex from 'crypto-js/enc-hex';
 
+const { UMI_APP_TG_BOT_TOKEN } = process.env;
+
 function sortQueryAlphabetically(query: LoginQuery) {
-  const keys = Object.keys(query);
+  const keys = Object.keys(query).filter(e => e !== 'hash');
   keys.sort((a, b) => a[0].localeCompare(b[0]));
   const splitter = '\n';
   const sortedString = keys
@@ -12,10 +14,14 @@ function sortQueryAlphabetically(query: LoginQuery) {
   return sortedString;
 }
 
-export function verifyTGLoginHash(query: LoginQuery) {
+export function verifyTGLoginHash(query?: LoginQuery) {
+  if (!query) {
+    throw new Error('INVALID_LOGIN_QUERY');
+  }
   const sortedString = sortQueryAlphabetically(query);
-  console.log(sortedString);
-
-  const bytes = hmacSHA256(sortedString, sha256(process.env.BOT_TOKEN as any));
+  if (!UMI_APP_TG_BOT_TOKEN) {
+    throw new Error('NO_BOT_TOKEN');
+  }
+  const bytes = hmacSHA256(sortedString, sha256(UMI_APP_TG_BOT_TOKEN));
   return bytes.toString(Hex);
 }
--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -1,20 +1,25 @@
+import { verifyTGLoginHash } from '@/lib';
 import { get, login } from '@/services';
 import { useRequest } from 'ahooks';
-import { Alert, Spin } from 'antd';
+import { Alert, Result, Spin } from 'antd';
 import parseUrl from 'parse-url';
 import { useEffect, useMemo } from 'react';
 import { useModel } from 'umi';
 
 export default function User() {
-  const { query } = useMemo(
-    () => parseUrl(window.location.href),
-    [window.location.href],
-  );
+  const { query, verified } = useMemo(() => {
+    const { query } = parseUrl(window.location.href);
+    const verificationHash = verifyTGLoginHash(query as LoginQuery);
+    return {
+      query,
+      verified: verificationHash === query.hash,
+    };
+  }, []);
   const { data: userInfo, loading: loading1 } = useRequest(
     () => login(query as LoginQuery),
     {
       refreshDeps: [query],
-      ready: !!query,
+      ready: !!query && verified,
       loadingDelay: 300,
     },
   );
@@ -22,7 +27,7 @@ export default function User() {
     () => get<UserData>('userdata', userInfo?.id),
     {
       refreshDeps: [userInfo?.id],
-      ready: !!userInfo?.id,
+      ready: !!userInfo?.id && verified,
       loadingDelay: 300,
     },
   );
@@ -37,14 +42,22 @@ export default function User() {
   return (
     <section>
       <p>User</p>
-      <Spin spinning={loading1 || loading2}>
-        {user ? <p>您已登录</p> : null}
-        {userData ? (
-          <p>石头余额：{userData.balance}</p>
-        ) : (
-          <Alert type="info" showIcon message="请通过姬器人签到以激活账户" />
-        )}
-      </Spin>
+      {verified ? (
+        <Spin spinning={loading1 || loading2}>
+          {user ? <p>您已登录</p> : null}
+          {userData ? (
+            <p>石头余额：{userData.balance}</p>
+          ) : (
+            <Alert type="info" showIcon message="请通过姬器人签到以激活账户" />
+          )}
+        </Spin>
+      ) : (
+        <Result
+          status="error"
+          title="您的登录凭据无法通过验证"
+          subTitle="如果您认为该结果有误，请联系管理员。"
+        />
+      )}
     </section>
   );
 }
